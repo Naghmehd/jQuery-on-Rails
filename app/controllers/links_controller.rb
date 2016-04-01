828a9e763e34bf8c7ce71e039b86cb3f03b95e74
Naghmeh Shirazi <naghmehd@gmail.com>
2016-04-01 12:40:38 -0500

    added form for links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,21 +4,27 @@ class LinksController < ApplicationController
     @links = Link.all.sort_by { |l| [l.scores, l.title] }.reverse!
   end
 
-  def show #Get
+  def show # Get
     link = Link.find(params[:id])
     if link
-        @link = link
+      @link = link
     else
-        render_not_found
+      render_not_found
     end
 
+    def create # link
 
+      Links.push(
+        link.new(params[:user_id], params[:title], params[:body], true)
+      )
+
+      redirect_to '/links'
+    end
   end
-
 
   private
 
   def render_not_found
-    render template: "links/not_found.html.erb"
+    render template: 'links/not_found.html.erb'
   end
 end
